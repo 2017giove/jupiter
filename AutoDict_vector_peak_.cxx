0348da2a7e21c2cab6737da7ee5411144991e922
GioVe <2017giove@gmail.com>
2017-06-15 20:07:55 +0200

    SPIKES

---
+++ AutoDict_vector_peak_.cxx
@@ -0,0 +1,12 @@
+#include "WaveAnalysis.h"
+#ifdef __CINT__ 
+#pragma link C++ nestedclasses;
+#pragma link C++ nestedtypedefs;
+#pragma link C++ class vector<peak>+;
+#pragma link C++ class vector<peak>::*;
+#ifdef G__VECTOR_HAS_CLASS_ITERATOR
+#pragma link C++ operators vector<peak>::iterator;
+#pragma link C++ operators vector<peak>::const_iterator;
+#pragma link C++ operators vector<peak>::reverse_iterator;
+#endif
+#endif
