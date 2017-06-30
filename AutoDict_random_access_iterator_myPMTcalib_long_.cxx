f15745d18301625d09fb8d8abd7628a0cf4f9091
GioVe <2017giove@gmail.com>
2017-06-30 18:03:53 +0200

    ordine

--- AutoDict_random_access_iterator_myPMTcalib_long_.cxx
+++
@@ -1,7 +0,0 @@
-#include "WaveAnalysis.h"
-#ifdef __CINT__ 
-#pragma link C++ nestedclasses;
-#pragma link C++ nestedtypedefs;
-#pragma link C++ class random_access_iterator<myPMTcalib,long>+;
-#pragma link C++ class random_access_iterator<myPMTcalib,long>::*+;
-#endif
